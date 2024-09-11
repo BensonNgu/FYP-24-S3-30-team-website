4a909b6e7e00fe70b0df9c5b8574fd0e4eda1a51
Benson Ngu <bensonngu25@gmail.com>
2024-09-11 20:32:55 +0800

    Update Styling

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Documents = () => {
     return (
-        <section id="documents" className="bg-gray-200 full-page-height py-12">
-        <div className="container mx-auto px-6 md:px-8">
+        <section id="documents" className="bg-stone-200 full-page-height py-12">
+        <div className="container mx-auto">
             <h1 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900 text-center">Research Details</h1>
             <p className="text-lg md:text-xl text-gray-700 text-center">
                 All the research details and progression will be posted here.
             </p>
+            <hr className="border-t-3 border-t-black my-4"/>
         </div>
     </section>
     );
--- src/components/TeamStructure.js
+++ src/components/TeamStructure.js
@@ -74,7 +74,7 @@ const TeamStructure = () => {
                     ))}
                 </div>
             </div>
-
+            <hr className="border-t-3 border-t-black mt-12 mx-5"/>
             <div id="team-structure" className="container mx-auto px-4 mt-12">
                 <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-gray-800">Team Structure</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
